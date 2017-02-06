0aa660d9789ea3cd743e8be943c7c4b620339ab5
StoriaNum <storia.numismatica@gmail.com>
2017-02-07 00:26:55 +0100

    Create items_1.js

---
+++ example/js/items_1.js
@@ -0,0 +1,47 @@
+// add items to the "Add Items" tab
+
+$(document).ready(function() {
+  var items = [
+  
+    {
+      "name" : "Tappeto",
+      "image" : "models/thumbnails/thumbnail_cb-blue-block60x96.png",
+      "model" : "models/js/cb-blue-block-60x96.js",
+      "type" : "8"
+    },
+    {
+      "name" : "Poster-Quadro-Foto",
+      "image" : "models/thumbnails/thumbnail_nyc2.jpg",
+      "model" : "models/js/nyc-poster2.js",
+      "type" : "2"
+    }
+   /*     
+   {
+      "name" : "",
+      "image" : "",
+      "model" : "",
+      "type" : "1"
+    }, 
+    */
+  ]
+
+
+
+  var itemsDiv = $("#items-wrapper")
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    var html = '<div class="col-sm-4">' +
+                '<a class="thumbnail add-item" model-name="' + 
+                item.name + 
+                '" model-url="' +
+                item.model +
+                '" model-type="' +
+                item.type + 
+                '"><img src="' +
+                item.image + 
+                '" alt="Add Item"> '+
+                item.name +
+                '</a></div>';
+    itemsDiv.append(html);
+  }
+});
