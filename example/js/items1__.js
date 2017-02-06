1fc717a00bdea1736a7d664d299b588b49464018
StoriaNum <storia.numismatica@gmail.com>
2017-02-06 14:59:48 +0100

    Create items1__.js

---
+++ example/js/items1__.js
@@ -0,0 +1,83 @@
+// add items to the "Add Items" tab
+
+$(document).ready(function() {
+  var items = [
+   {
+      "name" : "Porta Chiusa",
+      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
+      "model" : "models/js/closed-door28x80_baked.js",
+      "type" : "7"
+    }, 
+    {
+      "name" : "Porta Aperta",
+      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.22.46_PM.png",
+      "model" : "models/js/open_door.js",
+      "type" : "7"
+    }, 
+    {
+      "name" : "Finestra",
+      "image" : "models/thumbnails/thumbnail_window.png",
+      "model" : "models/js/whitewindow.js",
+      "type" : "3"
+    }, 
+    {
+      "name" : "Sedia",
+      "image" : "models/thumbnails/thumbnail_Church-Chair-oak-white_1024x1024.jpg",
+      "model" : "models/js/gus-churchchair-whiteoak.js",
+      "type" : "1"
+    }, 
+    {
+      "name" : "Sedia Colore Rosso",
+      "image" : "models/thumbnails/thumbnail_tn-orange.png",
+      "model" : "models/js/ik-ekero-orange_baked.js",
+      "type" : "1"
+    },
+    {
+      "name" : "Sedia Colore Blu",
+      "image" : "models/thumbnails/thumbnail_ekero-blue3.png",
+      "model" : "models/js/ik-ekero-blue_baked.js",
+      "type" : "1"
+    },
+    {
+      "name" : "Libreria",
+      "image" : "models/thumbnails/thumbnail_kendall-walnut-bookcase.jpg",
+      "model" : "models/js/cb-kendallbookcasewalnut_baked.js",
+      "type" : "1"
+    }, 
+  
+    {
+      "name" : "Poster-Quadro-Foto",
+      "image" : "models/thumbnails/thumbnail_nyc2.jpg",
+      "model" : "models/js/nyc-poster2.js",
+      "type" : "2"
+    }
+   /*     
+   {
+      "name" : "",
+      "image" : "",
+      "model" : "",
+      "type" : "1"
+    }, 
+    */
+  ]
+
+
+
+  var itemsDiv = $("#items-wrapper")
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    var html = '<div class="col-sm-4">' +
+                '<a class="thumbnail add-item" model-name="' + 
+                item.name + 
+                '" model-url="' +
+                item.model +
+                '" model-type="' +
+                item.type + 
+                '"><img src="' +
+                item.image + 
+                '" alt="Add Item"> '+
+                item.name +
+                '</a></div>';
+    itemsDiv.append(html);
+  }
+});
