eb0c417c05b1ebb5d2e28b2f145bd71fc91634ff
StoriaNum <storia.numismatica@gmail.com>
2017-02-12 16:33:18 +0100

    Update example.js

--- example/js/example.js
+++ example/js/example.js
@@ -1,3 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="html2canvas.js"></script>
+
 
 /*
  * Camera Buttons
@@ -509,6 +512,17 @@ var mainControls = function(blueprint3d) {
   }
   
   function saveJpg() {
+    html2canvas($('#viewer'), 
+    {
+      onrendered: function (canvas) {
+        var a = document.createElement('a');
+        // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
+        a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+        a.download = 'somefilename.jpg';
+        a.click();
+      }
+    });
+  });
   }   
 
   function init() {
