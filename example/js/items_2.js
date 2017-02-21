6283375bcd536e219308932449a002e38a4e5875
StoriaNum <storia.numismatica@gmail.com>
2017-02-21 14:03:19 +0100

    Update items_2.js

--- example/js/items_2.js
+++ example/js/items_2.js
@@ -7,24 +7,28 @@ $(document).ready(function() {
       "image" : "models/thumbnails/thumbnail_Blu-Dot-Shale-Bedside-Table.jpg",
       "model" : "models/js/bd-shalebedside-smoke_baked.js",
       "type" : "1"
+      "price" : "x&euro;"
     }, 
     {
       "name" : "Comodino Colore Bianco",
       "image" : "models/thumbnails/thumbnail_arch-white-oval-nightstand.jpg",
       "model" : "models/js/cb-archnight-white_baked.js",
       "type" : "1"
+      "price" : "x&euro;"
     },
     {
       "name" : "Consolle Luigi XV",
       "image" : "models/thumbnails/thumbnail_tavoloexp.png",
       "model" : "models/js/tavoloexp.js",
       "type" : "1"
+      "price" : "x&euro;"
     },
     {
       "name" : "Pouf Scoiattolo Colore Blue",
       "image" : "models/thumbnails/thumbnail_scoiattolopouf.png",
       "model" : "models/js/scoiattolopouf.js",
       "type" : "1"
+      "price" : "x&euro;"
     }
    /*     
    {
@@ -32,6 +36,7 @@ $(document).ready(function() {
       "image" : "",
       "model" : "",
       "type" : "1"
+      "price" : "x&euro;"
     }, 
     */
   ]
@@ -52,6 +57,7 @@ $(document).ready(function() {
                 item.image + 
                 '" alt="Add Item"> '+
                 item.name +
+               '<br>'+ item.price +
                 '</a></div>';
     itemsDiv_2.append(html);
   }
