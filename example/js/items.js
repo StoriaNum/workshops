6342e201628a62f56c7c56a2f82eeca127fc6048
StoriaNum <storia.numismatica@gmail.com>
2017-02-21 21:06:50 +0100

    Update items.js

--- example/js/items.js
+++ example/js/items.js
@@ -6,150 +6,175 @@ $(document).ready(function() {
       "name" : "Porta Chiusa",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
       "model" : "models/js/closed-door28x80_baked.js",
+      "price" : "x,y &euro;",
       "type" : "7"
     }, 
     {
       "name" : "Porta Aperta",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.22.46_PM.png",
       "model" : "models/js/open_door.js",
+      "price" : "x,y &euro;",
       "type" : "7"
     }, 
     {
       "name" : "Finestra",
       "image" : "models/thumbnails/thumbnail_window.png",
       "model" : "models/js/whitewindow.js",
+      "price" : "x,y &euro;",
       "type" : "3"
     }, 
     {
       "name" : "Sedia",
       "image" : "models/thumbnails/thumbnail_Church-Chair-oak-white_1024x1024.jpg",
       "model" : "models/js/gus-churchchair-whiteoak.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Sedia Colore Rosso",
       "image" : "models/thumbnails/thumbnail_tn-orange.png",
       "model" : "models/js/ik-ekero-orange_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     },
     {
       "name" : "Sedia Colore Blu",
       "image" : "models/thumbnails/thumbnail_ekero-blue3.png",
       "model" : "models/js/ik-ekero-blue_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     },
     {
       "name" : "Comò in Legno",
       "image" : "models/thumbnails/thumbnail_matera_dresser_5.png",
       "model" : "models/js/DWR_MATERA_DRESSER2.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Comò Colore Bianco",
       "image" : "models/thumbnails/thumbnail_img25o.jpg",
       "model" : "models/js/we-narrow6white_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     },  
     {
       "name" : "Comodino in Legno",
       "image" : "models/thumbnails/thumbnail_Blu-Dot-Shale-Bedside-Table.jpg",
       "model" : "models/js/bd-shalebedside-smoke_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Comodino Colore Bianco",
       "image" : "models/thumbnails/thumbnail_arch-white-oval-nightstand.jpg",
       "model" : "models/js/cb-archnight-white_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Guardaroba Colore Bianco",
       "image" : "models/thumbnails/thumbnail_TN-ikea-kvikine.png",
       "model" : "models/js/ik-kivine_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Letto Matrimoniale",
       "image" : "models/thumbnails/thumbnail_nordli-bed-frame__0159270_PE315708_S4.JPG",
       "model" : "models/js/ik_nordli_full.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Libreria",
       "image" : "models/thumbnails/thumbnail_kendall-walnut-bookcase.jpg",
       "model" : "models/js/cb-kendallbookcasewalnut_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
         {
       "name" : "Postazione Multimediale Colore Bianco",
       "image" : "models/thumbnails/thumbnail_clapboard-white-60-media-console-1.jpg",
       "model" : "models/js/cb-clapboard_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
         {
       "name" : "Postazione Multimediale Colore Nero",
       "image" : "models/thumbnails/thumbnail_moore-60-media-console-1.jpg",
       "model" : "models/js/cb-moore_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
        {
       "name" : "Divano Angolare Colore Verde",
       "image" : "models/thumbnails/thumbnail_img21o.jpg",
       "model" : "models/js/we-crosby2piece-greenbaked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Divano Colore Grigio",
       "image" : "models/thumbnails/thumbnail_rochelle-sofa-3.jpg",
       "model" : "models/js/cb-rochelle-gray_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
         {
       "name" : "Cassettiera in Legno",
       "image" : "models/thumbnails/thumbnail_teca-storage-trunk.jpg",
       "model" : "models/js/cb-tecs_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
         {
       "name" : "Lampada da Pavimento",
       "image" : "models/thumbnails/thumbnail_ore-white.png",
       "model" : "models/js/ore-3legged-white_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     },
     {
       "name" : "Tavolo Ellittico",
       "image" : "models/thumbnails/thumbnail_stockholm-coffee-table__0181245_PE332924_S4.JPG",
       "model" : "models/js/ik-stockholmcoffee-brown.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Tavolino",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-02-21_at_1.24.58_PM.png",
       "model" : "models/js/GUSossingtonendtable.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Tavolo da Pranzo Tondo",
       "image" : "models/thumbnails/thumbnail_scholar-dining-table.jpg",
       "model" : "models/js/cb-scholartable_baked.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     {
       "name" : "Tavolo da Pranzo Rettangolare-Quadrato",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-01-28_at_6.49.33_PM.png",
       "model" : "models/js/BlakeAvenuejoshuatreecheftable.js",
+      "price" : "x,y &euro;",
       "type" : "1"
     },
     {
       "name" : "Tappeto",
       "image" : "models/thumbnails/thumbnail_cb-blue-block60x96.png",
       "model" : "models/js/cb-blue-block-60x96.js",
+      "price" : "x,y &euro;",
       "type" : "8"
     },
     {
       "name" : "Poster-Quadro-Foto",
       "image" : "models/thumbnails/thumbnail_nyc2.jpg",
       "model" : "models/js/nyc-poster2.js",
+      "price" : "x,y &euro;",
       "type" : "2"
     }
    /*     
@@ -157,6 +182,7 @@ $(document).ready(function() {
       "name" : "",
       "image" : "",
       "model" : "",
+      "price" : "x,y &euro;",
       "type" : "1"
     }, 
     */
@@ -177,7 +203,8 @@ $(document).ready(function() {
                 '"><img src="' +
                 item.image + 
                 '" alt="Add Item"> '+
-                item.name +
+                item.name +  
+                '<br><i>Prezzo:</i> ' + item.price +
                 '</a></div>';
     itemsDiv.append(html);
   }
