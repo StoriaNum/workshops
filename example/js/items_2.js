e043ee8fc91de03e3a57f51d897e65c43571dfcd
StoriaNum <storia.numismatica@gmail.com>
2017-02-21 14:41:17 +0100

    Update items_2.js

--- example/js/items_2.js
+++ example/js/items_2.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
                 '"><img src="' +
                 item.image + 
                 '" alt="Add Item"> '+
-                item.name +
+                item.name + '<br>'
                 item.price +
                 '</a></div>';
     itemsDiv_2.append(html);
