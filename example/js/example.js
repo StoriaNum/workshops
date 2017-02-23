87ee25e16158a5b64e23028bb2c1e67c3067a301
StoriaNum <storia.numismatica@gmail.com>
2017-02-23 12:56:23 +0100

    Update example.js

--- example/js/example.js
+++ example/js/example.js
@@ -127,7 +127,7 @@ var ContextMenu = function(blueprint3d) {
         */
 
   //  var modelGalleryuri = "http://www.google.it";
-    var modelGalleryuri = $("model-gallery").text(item.metadata.modelGallery);
+    var modelGalleryuri = $("#modelGallery").text(item.metadata.modelGallery);
     document.getElementById("modelGallery").href = modelGalleryuri;
 
     
