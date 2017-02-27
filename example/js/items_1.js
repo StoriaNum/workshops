412e1f33b34b83e69d61ce7e9b57514835046fae
StoriaNum <storia.numismatica@gmail.com>
2017-02-27 15:50:32 +0100

    Update items_1.js

--- example/js/items_1.js
+++ example/js/items_1.js
@@ -47,6 +47,8 @@ $(document).ready(function() {
                 item.type + 
                 '" model-price="' +
                 item.price +
+                '" model-buybasket="' +
+                item.buybasket +
                 '" model-gallery="' + 
                 item.gallery +
                 '"><img src="' +
