2f43ba4b2dea8c9cdf4f17b81aca656189e1b478
StoriaNum <storia.numismatica@gmail.com>
2017-03-06 21:13:31 +0100

    Update cb-archnight-white_baked.js

--- example/models/js/cb-archnight-white_baked.js
+++ example/models/js/cb-archnight-white_baked.js
@@ -27,7 +27,7 @@
 		"colorSpecular" : [0.5, 0.5, 0.5],
 		"depthTest" : true,
 		"depthWrite" : true,
-		"mapDiffuse" : "cb-archnight-white_baked.png",
+		"mapDiffuse" : "../textures/cb-archnight-white_baked.png",
 		"mapDiffuseWrap" : ["repeat", "repeat"],
 		"shading" : "Lambert",
 		"specularCoef" : 50,
