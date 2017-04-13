0b88e3804cc9f8c5d97354d23293dd5fcfb84e91
StoriaNum <storia.numismatica@gmail.com>
2017-04-13 14:27:36 +0200

    Update items_2.js

--- example/js/items_2.js
+++ example/js/items_2.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
      {
       "name" : "Mobiletto tedesco",
       "image" : "models/thumbnails/thumbnails_tiefe60104.jpg",
-      "model" : "models/js/auc2.js",
+      "model" : "models/js/60_104A.js",
       "price" : "x,y &euro;",
       "buybasket" : "",
       "gallery" : "#",
