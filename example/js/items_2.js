0bcf3af263e68dc688e2e7875943d51f4679cd53
StoriaNum <storia.numismatica@gmail.com>
2017-05-08 15:02:36 +0200

    Update items_2.js

--- example/js/items_2.js
+++ example/js/items_2.js
@@ -128,6 +128,15 @@ $(document).ready(function() {
       "gallery" : "#",
       "type" : "1"
     },
+        {
+      "name" : "Mobiletto tedesco 6",
+      "image" : "models/thumbnails/thumbnail_780-1311_01.jpg",
+      "model" : "models/js/780-1311A.js",
+      "price" : "x,y &euro;",
+      "buybasket" : "",
+      "gallery" : "#",
+      "type" : "1"
+    },
     {
       "name" : "Fluttable",
       "image" : "models/thumbnails/thumbnail_tavoloexp.png",
