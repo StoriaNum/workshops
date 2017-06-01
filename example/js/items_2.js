511b7c67ab533fdaef0dfef948afac9afedc35b5
StoriaNum <storia.numismatica@gmail.com>
2017-06-01 18:46:00 +0200

    Update items_2.js

--- example/js/items_2.js
+++ example/js/items_2.js
@@ -389,6 +389,15 @@ $(document).ready(function() {
       "gallery" : "#",
       "type" : "1"
     },
+             {
+      "name" : "Tavolo a crescente tedesco 00",
+      "image" : "models/thumbnails/5120494.jpg",
+      "model" : "models/js/Howe_Tempest_Flip_Cresent_1500x750.js",
+      "price" : "x,y &euro;",
+      "buybasket" : "",
+      "gallery" : "#",
+      "type" : "1"
+    },
     {
       "name" : "Fluttable",
       "image" : "models/thumbnails/thumbnail_tavoloexp.png",
