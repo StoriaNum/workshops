e630b9ec3557e47d6d9c76276b1e9e3c44fe8d13
StoriaNum <storia.numismatica@gmail.com>
2017-08-31 17:31:27 +0200

    Update items.js

--- example/js/items.js
+++ example/js/items.js
@@ -2,7 +2,8 @@
 
 $(document).ready(function() {
   var items = [
-   {
+   
+    {
       "name" : "Porta Chiusa",
       "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
       "model" : "models/js/closed-door28x80_baked.js",
@@ -37,8 +38,8 @@ $(document).ready(function() {
       "buybasket" : "",
       "gallery": "#",
       "type" : "1"
-    }, 
-    {
+    },
+ {
       "name" : "Sedia Colore Rosso",
       "image" : "models/thumbnails/thumbnail_tn-orange.png",
       "model" : "models/js/ik-ekero-orange_baked.js",
@@ -56,177 +57,44 @@ $(document).ready(function() {
       "gallery": "#",
       "type" : "1"
     },
-    {
-      "name" : "Comò in Legno",
-      "image" : "models/thumbnails/thumbnail_matera_dresser_5.png",
-      "model" : "models/js/DWR_MATERA_DRESSER2.js",
+{
+      "name" : "Bliss Rovere con Tessuto Grigio",
+      "image" : "models/thumbnails/thumbnail_bliss_rovere.jpg",
+      "model" : "models/js/bliss1.js",
       "price" : "x,y &euro;",
       "buybasket" : "",
-      "gallery": "#",
+      "gallery" : "#",
       "type" : "1"
-    }, 
+    },
     {
-      "name" : "Comò Colore Bianco",
-      "image" : "models/thumbnails/thumbnail_img25o.jpg",
-      "model" : "models/js/we-narrow6white_baked.js",
+      "name" : "Bliss Nero Lucido con Ecopelle",
+      "image" : "models/thumbnails/thumbnail_bliss_pelle-nerolucido.jpg",
+      "model" : "models/js/bliss2.js",
       "price" : "x,y &euro;",
       "buybasket" : "",
-      "gallery": "#",
+      "gallery" : "#",
+      "type" : "1"
+    },    
+        {
+      "name" : "Bliss con braccioli blu e nero",
+      "image" : "models/thumbnails/thumbnail_blisswithoutblend.jpg",
+      "model" : "models/js/blisswithoutblend.js",
+      "price" : "x,y &euro;",
+      "buybasket" : "",
+      "gallery" : "#",
       "type" : "1"
     },  
-    {
-      "name" : "Comodino in Legno",
-      "image" : "models/thumbnails/thumbnail_Blu-Dot-Shale-Bedside-Table.jpg",
-      "model" : "models/js/bd-shalebedside-smoke_baked.js",
+     {
+      "name" : "C1 nero e acciaio",
+      "image" : "models/thumbnails/thumbnail_blisswithoutblend.jpg",
+      "model" : "models/js/c1vitra.js",
       "price" : "x,y &euro;",
       "buybasket" : "",
-      "gallery": "#",
+      "gallery" : "#",
       "type" : "1"
-    }, 
-    {
-      "name" : "Comodino Colore Bianco",
-      "image" : "models/thumbnails/thumbnail_arch-white-oval-nightstand.jpg",
-      "model" : "models/js/cb-archnight-white_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Guardaroba Colore Bianco",
-      "image" : "models/thumbnails/thumbnail_TN-ikea-kvikine.png",
-      "model" : "models/js/ik-kivine_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Letto Matrimoniale",
-      "image" : "models/thumbnails/thumbnail_nordli-bed-frame__0159270_PE315708_S4.JPG",
-      "model" : "models/js/ik_nordli_full.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Libreria",
-      "image" : "models/thumbnails/thumbnail_kendall-walnut-bookcase.jpg",
-      "model" : "models/js/cb-kendallbookcasewalnut_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-        {
-      "name" : "Postazione Multimediale Colore Bianco",
-      "image" : "models/thumbnails/thumbnail_clapboard-white-60-media-console-1.jpg",
-      "model" : "models/js/cb-clapboard_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-        {
-      "name" : "Postazione Multimediale Colore Nero",
-      "image" : "models/thumbnails/thumbnail_moore-60-media-console-1.jpg",
-      "model" : "models/js/cb-moore_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-       {
-      "name" : "Divano Angolare Colore Verde",
-      "image" : "models/thumbnails/thumbnail_img21o.jpg",
-      "model" : "models/js/we-crosby2piece-greenbaked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Divano Colore Grigio",
-      "image" : "models/thumbnails/thumbnail_rochelle-sofa-3.jpg",
-      "model" : "models/js/cb-rochelle-gray_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-        {
-      "name" : "Cassettiera in Legno",
-      "image" : "models/thumbnails/thumbnail_teca-storage-trunk.jpg",
-      "model" : "models/js/cb-tecs_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-        {
-      "name" : "Lampada da Pavimento",
-      "image" : "models/thumbnails/thumbnail_ore-white.png",
-      "model" : "models/js/ore-3legged-white_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    },
-    {
-      "name" : "Tavolo Ellittico",
-      "image" : "models/thumbnails/thumbnail_stockholm-coffee-table__0181245_PE332924_S4.JPG",
-      "model" : "models/js/ik-stockholmcoffee-brown.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Tavolino",
-      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-02-21_at_1.24.58_PM.png",
-      "model" : "models/js/GUSossingtonendtable.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Tavolo da Pranzo Tondo",
-      "image" : "models/thumbnails/thumbnail_scholar-dining-table.jpg",
-      "model" : "models/js/cb-scholartable_baked.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Tavolo da Pranzo Rettangolare-Quadrato",
-      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-01-28_at_6.49.33_PM.png",
-      "model" : "models/js/BlakeAvenuejoshuatreecheftable.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "1"
-    },
-    {
-      "name" : "Tappeto",
-      "image" : "models/thumbnails/thumbnail_cb-blue-block60x96.png",
-      "model" : "models/js/cb-blue-block-60x96.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "8"
-    },
-    {
-      "name" : "Poster-Quadro-Foto",
-      "image" : "models/thumbnails/thumbnail_nyc2.jpg",
-      "model" : "models/js/nyc-poster2.js",
-      "price" : "x,y &euro;",
-      "buybasket" : "",
-      "gallery": "#",
-      "type" : "2"
     }
+    
+    
    /*     
    {
       "name" : "",
