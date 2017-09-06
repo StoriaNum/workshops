22a08500bd7fc3a256f4df8ad4e01b6e519e0195
StoriaNum <storia.numismatica@gmail.com>
2017-09-06 19:46:33 +0200

    Update example.js

--- example/js/example.js
+++ example/js/example.js
@@ -151,7 +151,7 @@ var ContextMenu = function(blueprint3d) {
   }
 
     
-    if (selettoreifcustom === "CUSTOM") { 
+   else if (selettoreifcustom === "CUSTOM") { 
                                             
                                             
                                             document.getElementById("infoiva").setAttribute("hidden", true);
