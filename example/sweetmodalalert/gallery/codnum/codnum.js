0ad45f2f98154c53097d186c663eb218fbc085c1
StoriaNum <storia.numismatica@gmail.com>
2017-09-11 13:10:04 +0200

    Update codnum.js

--- example/sweetmodalalert/gallery/codnum/codnum.js
+++ example/sweetmodalalert/gallery/codnum/codnum.js
@@ -15,8 +15,8 @@ function Functioncodnum() {
 	},
 
 	content: {
-		tab1: '<center><img src="http://mediametalli.altervista.org/sismacentroitalia/page/crocesmom.PNG" alt="cod " id="imgtab1"></img> <script>$(document).ready(function(){ $("#imgtab1").width(200); $("#imgtab1").mouseover(function() { $(this).css("cursor","pointer"); $(this).animate({width: "500px"}, "slow"); }); $("#imgtab1").mouseout(function() { $(this).animate({width: "200px"}, "slow"); }); });</script></center>',
-		tab2: '<center><img src="http://mediametalli.altervista.org/underconstruction.jpg" alt="cod " id="imgtab2"></img> <script>$(document).ready(function(){ $("#imgtab2").width(200); $("#imgtab2").mouseover(function() { $(this).css("cursor","pointer"); $(this).animate({width: "500px"}, "slow"); }); $("#imgtab2").mouseout(function() { $(this).animate({width: "200px"}, "slow"); }); });</script></center>',
+		tab1: '<center><img src="crocesmom.PNG" alt="cod " id="imgtab1"></img> <script>$(document).ready(function(){ $("#imgtab1").width(200); $("#imgtab1").mouseover(function() { $(this).css("cursor","pointer"); $(this).animate({width: "500px"}, "slow"); }); $("#imgtab1").mouseout(function() { $(this).animate({width: "200px"}, "slow"); }); });</script></center>',
+		tab2: '<center><img src="underconstruction.jpg" alt="cod " id="imgtab2"></img> <script>$(document).ready(function(){ $("#imgtab2").width(200); $("#imgtab2").mouseover(function() { $(this).css("cursor","pointer"); $(this).animate({width: "500px"}, "slow"); }); $("#imgtab2").mouseout(function() { $(this).animate({width: "200px"}, "slow"); }); });</script></center>',
         tab3: 'Colori disponibili...'
 	},
 /*
