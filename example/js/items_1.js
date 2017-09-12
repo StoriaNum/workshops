3c83cd7fb123d4fab4652bd0e676cf928c56e9a1
StoriaNum <storia.numismatica@gmail.com>
2017-09-12 02:17:56 +0200

    Update items_1.js

--- example/js/items_1.js
+++ example/js/items_1.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
       "model" : "models/js/Howe_Tempest_Flip_Cresent_1500x750.js",
       "price" : "x,y &euro;",
       "buybasket" : "",
-      "gallery" : "https://storianum.github.io/workshops/example/sweetmodalalert/gallery/codnum/codnum.js",
+      "gallery" : "http://mediametalli.altervista.org/sweetmodalalert/gallery/codnum/codnum.html",
       "type" : "1"
     },
 {
