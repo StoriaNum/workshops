6ddda88ea3686b484b36d03d6fd5a3b0a267f912
StoriaNum <storia.numismatica@gmail.com>
2017-10-09 13:10:51 +0200

    Update items_5.js

--- example/js/items_5.js
+++ example/js/items_5.js
@@ -11,7 +11,16 @@ $(document).ready(function() {
       "gallery" : "#",
       "type" : "1"
     },
-         {
+    {
+      "name" : "Scale tipo 7",
+      "image" : "https://storianum.github.io/repoworkshops/example/models/thumbnails/thumbnail_scale7.jpg",
+      "model" : "https://storianum.github.io/repoworkshops/example/models/js/scale7.js",
+      "price" : "NOT-ON-SALE",
+      "buybasket" : "",
+      "gallery": "#",
+      "type" : "1"
+    },
+     {
       "name" : "Scaffalatura 01",
       "image" : "models/thumbnails/hoehe-2000-mm-6-ebenen.jpg",
       "model" : "models/js/HRG20610DA_07.js",
