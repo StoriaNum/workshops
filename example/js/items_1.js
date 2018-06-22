88df8f772cd2b4660bbd46149e55327aee204853
StoriaNum <storia.numismatica@gmail.com>
2018-06-22 16:38:02 +0200

    Update items_1.js

--- example/js/items_1.js
+++ example/js/items_1.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
       {
       "name" : "Tavolo Asteria",
       "image" : "models/thumbnails/thumbnail_-asteria-temp.jpg",
-      "model" : "models/js/asteria.js",
+      "model" : "models/js/asteria2.js",
       "price" : "2750,00 &euro;",
       "buybasket" : "",
       "gallery": "#",
