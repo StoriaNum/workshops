465ce4bb3ae3f5e345d7ce7faf7fdb02b0bdbb9e
StoriaNum <storia.numismatica@gmail.com>
2018-07-13 15:37:52 +0200

    Update items_1.js

--- example/js/items_1.js
+++ example/js/items_1.js
@@ -194,9 +194,29 @@ $(document).ready(function() {
       "type" : "1"
     },
         {
-      "name" : "Tavolo Levana  SU MISURA",
+      "name" : "Tavolo Levana SU MISURA",
       "image" : "models/thumbnails/thumbnail_levana.jpg",
       "model" : "models/js/levana.js",
+      "price" : "CUSTOM:A-RICHIESTA",
+      "buybasket" : "",
+      "gallery": "#",
+      "datar": "#",
+      "type" : "1"  
+    },
+       {
+      "name" : "Tavolo Vessinna",
+      "image" : "models/thumbnails/thumbnail_vessinna.jpg",
+      "model" : "models/js/vessinna.js",
+      "price" : "0,00 &euro;",
+      "buybasket" : "",
+      "gallery": "#",
+      "datar": "Sx=0.006&Sy=0.009&Sz=0.008&Px=-1&Py=0&D=10:-0.2!-0!0.2!0.5!0.75",
+      "type" : "1"
+    },
+        {
+      "name" : "Tavolo Vessinna SU MISURA",
+      "image" : "models/thumbnails/thumbnail_vessinna.jpg",
+      "model" : "models/js/vessinna.js",
       "price" : "CUSTOM:A-RICHIESTA",
       "buybasket" : "",
       "gallery": "#",
