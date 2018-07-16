f3e17a37d64120a5093487ba2e70f7016d82acb7
StoriaNum <storia.numismatica@gmail.com>
2018-07-16 12:33:41 +0200

    Update items_1.js

--- example/js/items_1.js
+++ example/js/items_1.js
@@ -223,6 +223,26 @@ $(document).ready(function() {
       "datar": "#",
       "type" : "1"  
     },
+       {
+      "name" : "Tavolo Epona",
+      "image" : "models/thumbnails/thumbnail_epona.jpg",
+      "model" : "models/js/epona.js",
+      "price" : "0,00 &euro;",
+      "buybasket" : "",
+      "gallery": "#",
+      "datar": "Sx=0.006&Sy=0.009&Sz=0.008&Px=-1&Py=0&D=10:-0.2!-0!0.2!0.5!0.75",
+      "type" : "1"
+    },
+        {
+      "name" : "Tavolo Epona SU MISURA",
+      "image" : "models/thumbnails/thumbnail_epona.jpg",
+      "model" : "models/js/epona.js",
+      "price" : "CUSTOM:A-RICHIESTA",
+      "buybasket" : "",
+      "gallery": "#",
+      "datar": "#",
+      "type" : "1"  
+    },
 {
       "name" : "Tavolo a crescente 00",
       "image" : "models/thumbnails/51204940.jpg",
