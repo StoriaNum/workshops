1cde71d70b18207488ff89b1fdde3d0597a64bef
StoriaNum <storia.numismatica@gmail.com>
2018-07-31 15:45:51 +0200

    Update items_1.js

--- example/js/items_1.js
+++ example/js/items_1.js
@@ -363,7 +363,27 @@ $(document).ready(function() {
       "datar": "#",
       "type" : "1"  
     },
-{
+    {
+      "name" : "Tavolo da conferenza Imperium",
+      "image" : "models/thumbnails/thumbnail_imperium.jpg",
+      "model" : "models/js/imperium.js",
+      "price" : "0,00 &euro;",
+      "buybasket" : "",
+      "gallery": "#",
+      "datar": "Sx=0.006&Sy=0.009&Sz=0.008&Px=-1&Py=0&D=10:-0.2!-0!0.2!0.5!0.75",
+      "type" : "1"
+    },
+        {
+      "name" : "Tavolo Imperium SU MISURA",
+      "image" : "models/thumbnails/thumbnail_imperium.jpg",
+      "model" : "models/js/imperium.js",
+      "price" : "CUSTOM:A-RICHIESTA",
+      "buybasket" : "",
+      "gallery": "#",
+      "datar": "#",
+      "type" : "1"  
+    },
+    {
       "name" : "Tavolo a crescente 00",
       "image" : "models/thumbnails/51204940.jpg",
       "model" : "models/js/Howe_Tempest_Flip_Cresent_1500x750.js",
